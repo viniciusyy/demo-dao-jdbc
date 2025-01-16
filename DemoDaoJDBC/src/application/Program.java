53972b28aa0c6a1648995d00516cab70909d0886
Vinicius Miyata <viyudi@hotmail.com>
2025-01-16 18:14:49 -0300

    findyById implementation

--- DemoDaoJDBC/src/application/Program.java
+++ DemoDaoJDBC/src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,12 +10,12 @@ public class Program {
 
 	public static void main(String[] args) {
 	
-
-		Department obj = new Department(1, "Books");
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
 
 	}
--- DemoDaoJDBC/src/module-info.java
+++ DemoDaoJDBC/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module DemoDaoJDBC {
+	requires java.sql;
 }
